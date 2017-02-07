63581068d050fd406dc3d21ccb2fe3ee1a50993f
netfusion <terrorhund.the.chilla@hotmail.de>
2017-02-07 18:11:45 +0100

    added missing unary command

--- lang/unary-commands.js
+++ lang/unary-commands.js
@@ -188,7 +188,7 @@ module.exports = [
     'secondaryWeapon',
     'secondaryWeaponItems',
     'secondaryWeaponMagazine',
-    'select',
+    'selectMax',
     'selectPlayer',
     'selectRandom',
     'serverCommandAvailable',
