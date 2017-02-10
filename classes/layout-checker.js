bce5017adb213c559400905c066ded58c4b27e69
netfusion <terrorhund.the.chilla@hotmail.de>
2017-02-10 02:57:49 +0100

    added layout checker

---
+++ classes/layout-checker.js
@@ -0,0 +1,34 @@
+/*
+ DO NOT USE WITHOUT EXCLUSIVE PERMISSION
+
+ SQFLint
+ Author: NetFusion
+ https://github.com/netfusion/SQFLint
+
+ Description:
+ Parser class for SQFLint
+ */
+
+const Parser = require('./parser');
+
+class LayoutChecker extends Parser {
+    static checkLayout(tokens, filename, content) {
+        const layoutChecker = new LayoutChecker(tokens, filename, content);
+        try {
+            layoutChecker.check();
+        } catch (e) {
+            return 1;
+        }
+        return 0;
+    }
+
+    check() {
+        const macroInclude = this.expect('preprocessor-include');
+        if (macroInclude.value != 'macros.hpp') this.error('Unknown include path: {0}', macroInclude.value);
+        this.expect('newline');
+        const fileHeader = this.expect('blockComment');
+        //console.log(fileHeader);
+    }
+}
+
+module.exports = LayoutChecker;
